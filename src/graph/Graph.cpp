a7bd0f1ad6d270b379e9dcddaf5c40a38cdcc888
Dmitriy Zolotykh <dm.zolotykh@gmail.com>
2024-02-21 23:00:21 +0300

    implemented graph class

--- src/graph/Graph.cpp
+++ src/graph/Graph.cpp
@@ -1,1 +1,27 @@
 #include "Graph.h"
+#include <iostream>
+#include <fstream>
+
+Graph::Graph(const std::string &path) {
+    std::ifstream fs(path);
+    if (!fs.is_open()) {
+        throw std::runtime_error("Cannot open the file " + path);
+    }
+    size_t vertex_num, edges_num;
+    fs >> vertex_num >> edges_num;
+    g_.resize(vertex_num);
+    for (size_t i = 0; i < edges_num; ++i) {
+        Graph::vertexT u, v;
+        fs >> u >> v;
+        g_[u].push_back(v);
+        g_[v].push_back(u);
+    }
+}
+
+const std::vector<Graph::vertexT> &Graph::get_neighbours(Graph::vertexT vertex_num) const noexcept {
+    return g_[vertex_num];
+}
+
+size_t Graph::size() const noexcept {
+    return g_.size();
+}
--- src/graph/Graph.h
+++ src/graph/Graph.h
@@ -1,9 +1,19 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#include <vector>
 
 class Graph {
+public:
+    using vertexT = size_t;
+    size_t size() const noexcept;
+    const std::vector<vertexT>& get_neighbours(vertexT vertex_num) const noexcept;
+    explicit Graph(const std::string &path);
 
+private:
+    using containerT = std::vector<std::vector<size_t>>;
+
+    containerT g_;
 };
 
 
