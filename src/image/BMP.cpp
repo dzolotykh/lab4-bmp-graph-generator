e4eb3e4482117d9fab739d6fc246d3b645beb04b
Dmitriy Zolotykh <dm.zolotykh@gmail.com>
2024-02-21 20:52:46 +0300

    implemented realisation for BMP class

--- src/image/BMP.cpp
+++ src/image/BMP.cpp
@@ -1,1 +1,94 @@
 #include "BMP.h"
+
+BMP::BMP(size_t _width, size_t _height)
+    : width(_width), height(_height), pixels(_width * _height * 3) {
+    std::fill(pixels.begin(), pixels.end(), 255);
+}
+
+void BMP::save(const std::string& path) const noexcept {
+    std::ofstream outputFile(path, std::ios::binary);
+    for (auto i : Header(width, height).get_bytes()) {
+        outputFile.write(i.first, i.second);
+    }
+    outputFile.write(reinterpret_cast<const char*>(pixels.data()), Header(width, height).data_size);
+    outputFile.close();
+};
+
+void BMP::draw_pixel(size_t x, size_t y, u_char r, u_char g, u_char b) noexcept {
+    if (get_pos(x, y) + 2 >= pixels.size())
+        return;
+    pixels[get_pos(x, y)] = r;
+    pixels[get_pos(x, y) + 1] = g;
+    pixels[get_pos(x, y) + 2] = b;
+}
+
+void BMP::draw_segment(int x1, int y1, int x2, int y2) noexcept {
+    int x, y, dx, dy, dx1, dy1, px, py, xe, ye, i;
+    dx = x2 - x1;
+    dy = y2 - y1;
+    dx1 = std::abs(dx);
+    dy1 = std::abs(dy);
+    px = 2 * dy1 - dx1;
+    py = 2 * dx1 - dy1;
+    if (dy1 <= dx1) {
+        if (dx >= 0) {
+            x = x1;
+            y = y1;
+            xe = x2;
+        } else {
+            x = x2;
+            y = y2;
+            xe = x1;
+        }
+        draw_pixel(x, y);
+        for (i = 0; x < xe; i++) {
+            x = x + 1;
+            if (px < 0) {
+                px = px + 2 * dy1;
+            } else {
+                if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0)) {
+                    y = y + 1;
+                } else {
+                    y = y - 1;
+                }
+                px = px + 2 * (dy1 - dx1);
+            }
+            draw_pixel(x, y);
+        }
+    } else {
+        if (dy >= 0) {
+            x = x1;
+            y = y1;
+            ye = y2;
+        } else {
+            x = x2;
+            y = y2;
+            ye = y1;
+        }
+        draw_pixel(x, y);
+        for (i = 0; y < ye; i++) {
+            y = y + 1;
+            if (py <= 0) {
+                py = py + 2 * dx1;
+            } else {
+                if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0)) {
+                    x = x + 1;
+                } else {
+                    x = x - 1;
+                }
+                py = py + 2 * (dx1 - dy1);
+            }
+            draw_pixel(x, y);
+        }
+    }
+}
+
+void BMP::draw_circle(int x1, int y1, int r) noexcept {
+    for (int x = x1 - r; x <= x1 + r; x++) {
+        for (int y = y1 - r; y <= y1 + r; y++) {
+            if ((x1 - x) * (x1 - x) + (y1 - y) * (y1 - y) < r * r) {
+                draw_pixel(x, y);
+            }
+        }
+    }
+}
--- src/image/BMP.h
+++ src/image/BMP.h
@@ -1,10 +1,76 @@
 #ifndef BMP_H
 #define BMP_H
 
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <vector>
 
 class BMP {
+   public:
+    BMP(size_t _width, size_t _height);
 
+    void save(const std::string& path) const noexcept;
+
+    // sets pixel in point (x, y) in color (r, g, b)
+    void draw_pixel(size_t x, size_t y, u_char r = 0, u_char g = 0, u_char b = 0) noexcept;
+
+    void draw_circle(int x1, int y1, int r) noexcept;
+
+    void draw_segment(int x1, int y1, int x2, int y2) noexcept;
+
+   private:
+    std::vector<uint8_t> pixels;
+    const size_t width, height;
+
+    size_t get_pos(size_t x, size_t y) const noexcept { return (x * height + y) * 3; }
+
+    struct Header {
+        std::string encoding_type = "BM";
+        int file_size;
+        size_t width;
+        size_t height;
+        std::string reserved = "\0\0\0\0";
+        int data_offset = 54;
+        int header_size = 40;
+        short planes = 1;
+        short bpp = 24;
+        int compression = 0;
+        int data_size;
+        int h_resolution = 0;
+        int v_resolution = 0;
+        int colors = 0;
+        int importantColors = 0;
+
+        Header(size_t _width, size_t _height)
+            : width(_width),
+              height(_height),
+              data_size(3 * _width * _height),
+              file_size(54 + 3 * _width * _height) {}
+
+        std::vector<std::pair<const char*, int>> get_bytes() const noexcept {
+            std::vector<std::pair<const char*, int>> bytes_arr;
+
+            bytes_arr.emplace_back(reinterpret_cast<const char*>(&encoding_type), 2);
+            bytes_arr.emplace_back(reinterpret_cast<const char*>(&file_size), 4);
+            bytes_arr.emplace_back(reinterpret_cast<const char*>(&reserved), 4);
+            bytes_arr.emplace_back(reinterpret_cast<const char*>(&data_offset), 4);
+            bytes_arr.emplace_back(reinterpret_cast<const char*>(&header_size), 4);
+            bytes_arr.emplace_back(reinterpret_cast<const char*>(&width), 4);
+            bytes_arr.emplace_back(reinterpret_cast<const char*>(&height), 4);
+            bytes_arr.emplace_back(reinterpret_cast<const char*>(&planes), 2);
+            bytes_arr.emplace_back(reinterpret_cast<const char*>(&bpp), 2);
+            bytes_arr.emplace_back(reinterpret_cast<const char*>(&compression), 4);
+            bytes_arr.emplace_back(reinterpret_cast<const char*>(&data_size), 4);
+            bytes_arr.emplace_back(reinterpret_cast<const char*>(&h_resolution), 4);
+            bytes_arr.emplace_back(reinterpret_cast<const char*>(&v_resolution), 4);
+            bytes_arr.emplace_back(reinterpret_cast<const char*>(&colors), 4);
+            bytes_arr.emplace_back(reinterpret_cast<const char*>(&importantColors), 4);
+
+            return bytes_arr;
+        }
+    };
 };
 
-
-#endif // BMP_H
+#endif  // BMP_H
