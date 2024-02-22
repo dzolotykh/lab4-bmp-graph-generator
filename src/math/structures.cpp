5b6267e95f13ea4ee8b9dda5e17bcecdad8198b6
Dmitriy Zolotykh <dm.zolotykh@gmail.com>
2024-02-22 13:15:53 +0300

    codestyle changes

--- src/math/structures.cpp
+++ src/math/structures.cpp
@@ -1,41 +1,43 @@
 #include "structures.h"
 #include <cmath>
 
-Vector2D operator+(const Vector2D &l, const Vector2D &r) noexcept {
+Vector2D operator+(const Vector2D& l, const Vector2D& r) noexcept {
     return Vector2D(l.x + r.x, l.y + r.y);
 }
 
-Vector2D operator-(const Vector2D &l, const Vector2D &r) noexcept {
+Vector2D operator-(const Vector2D& l, const Vector2D& r) noexcept {
     return Vector2D(l.x - r.x, l.y - r.y);
 }
 
-Vector2D operator*(const Vector2D &l, double scalar) noexcept {
+Vector2D operator*(const Vector2D& l, double scalar) noexcept {
     return Vector2D(l.x * scalar, l.y * scalar);
 }
 
-Vector2D operator*(double scalar, const Vector2D &r) noexcept {
+Vector2D operator*(double scalar, const Vector2D& r) noexcept {
     return Vector2D(r.x * scalar, r.y * scalar);
 }
 
-Vector2D operator/(const Vector2D &l, double scalar) noexcept {
+Vector2D operator/(const Vector2D& l, double scalar) noexcept {
     if (scalar == 0) {
         double _x = std::numeric_limits<double>::max();
-        if (l.x < 0) _x *= -1;
+        if (l.x < 0)
+            _x *= -1;
         double _y = std::numeric_limits<double>::max();
-        if (l.y < 0) _y *= -1;
+        if (l.y < 0)
+            _y *= -1;
         return Vector2D(_x, _y);
     }
 
     return Vector2D(l.x / scalar, l.y / scalar);
 }
 
-Vector2D& Vector2D::operator+=(const Vector2D &other) noexcept {
+Vector2D& Vector2D::operator+=(const Vector2D& other) noexcept {
     x += other.x;
     y += other.y;
     return *this;
 }
 
-Vector2D& Vector2D::operator-=(const Vector2D &other) noexcept {
+Vector2D& Vector2D::operator-=(const Vector2D& other) noexcept {
     x -= other.x;
     y -= other.y;
     return *this;
@@ -50,9 +52,11 @@ Vector2D& Vector2D::operator*=(double scalar) noexcept {
 Vector2D& Vector2D::operator/=(double scalar) noexcept {
     if (scalar == 0) {
         double _x = std::numeric_limits<double>::max();
-        if (x < 0) _x *= -1;
+        if (x < 0)
+            _x *= -1;
         double _y = std::numeric_limits<double>::max();
-        if (y < 0) _y *= -1;
+        if (y < 0)
+            _y *= -1;
         x = _x;
         y = _y;
     } else {
@@ -66,15 +70,15 @@ double Vector2D::size() const noexcept {
     return std::sqrt(x * x + y * y);
 }
 
-Point2D operator+(const Point2D &l, const Vector2D &s) noexcept {
+Point2D operator+(const Point2D& l, const Vector2D& s) noexcept {
     return Point2D(l.x + s.x, l.y + s.y);
 }
 
-Point2D operator-(const Point2D &l, const Vector2D &s) noexcept {
+Point2D operator-(const Point2D& l, const Vector2D& s) noexcept {
     return Point2D(l.x - s.x, l.y - s.y);
 }
 
-Vector2D operator-(const Point2D &l, const Point2D &s) noexcept {
+Vector2D operator-(const Point2D& l, const Point2D& s) noexcept {
     return Vector2D(l.x - s.x, l.y - s.y);
 }
 
@@ -82,13 +86,13 @@ Point2D::operator Vector2D() const noexcept {
     return Vector2D(x, y);
 }
 
-Point2D& Point2D::operator+=(const Vector2D &other) noexcept {
+Point2D& Point2D::operator+=(const Vector2D& other) noexcept {
     x += other.x;
     y += other.y;
     return *this;
 }
 
-Point2D& Point2D::operator-=(const Vector2D &other) noexcept {
+Point2D& Point2D::operator-=(const Vector2D& other) noexcept {
     x -= other.x;
     y -= other.y;
     return *this;
--- src/math/structures.h
+++ src/math/structures.h
@@ -6,13 +6,13 @@ struct Point2D;
 struct Vector2D {
     double x, y;
 
-    explicit Vector2D(double _x = 0, double _y = 0) noexcept: x(_x), y(_y) {}
+    explicit Vector2D(double _x = 0, double _y = 0) noexcept : x(_x), y(_y) {}
 
     explicit operator Point2D() const noexcept;
 
-    Vector2D& operator+=(const Vector2D &other) noexcept;
+    Vector2D& operator+=(const Vector2D& other) noexcept;
 
-    Vector2D& operator-=(const Vector2D &other) noexcept;
+    Vector2D& operator-=(const Vector2D& other) noexcept;
 
     Vector2D& operator*=(double scalar) noexcept;
 
@@ -21,32 +21,31 @@ struct Vector2D {
     double size() const noexcept;
 };
 
-Vector2D operator+(const Vector2D &l, const Vector2D &r) noexcept;
+Vector2D operator+(const Vector2D& l, const Vector2D& r) noexcept;
 
-Vector2D operator-(const Vector2D &l, const Vector2D &r) noexcept;
+Vector2D operator-(const Vector2D& l, const Vector2D& r) noexcept;
 
-Vector2D operator*(const Vector2D &l, double scalar) noexcept;
+Vector2D operator*(const Vector2D& l, double scalar) noexcept;
 
-Vector2D operator*(double scalar, const Vector2D &r) noexcept;
-Vector2D operator/(const Vector2D &l, double scalar) noexcept;
+Vector2D operator*(double scalar, const Vector2D& r) noexcept;
+Vector2D operator/(const Vector2D& l, double scalar) noexcept;
 
 struct Point2D {
     double x, y;
 
-    explicit Point2D(double _x = 0, double _y = 0): x(_x), y(_y) {}
+    explicit Point2D(double _x = 0, double _y = 0) : x(_x), y(_y) {}
 
     explicit operator Vector2D() const noexcept;
 
-    Point2D& operator+=(const Vector2D &other) noexcept;
+    Point2D& operator+=(const Vector2D& other) noexcept;
 
-    Point2D& operator-=(const Vector2D &other) noexcept;
+    Point2D& operator-=(const Vector2D& other) noexcept;
 };
 
-Point2D operator+(const Point2D &l, const Vector2D &s) noexcept;
+Point2D operator+(const Point2D& l, const Vector2D& s) noexcept;
 
-Point2D operator-(const Point2D &l, const Vector2D &s) noexcept;
+Point2D operator-(const Point2D& l, const Vector2D& s) noexcept;
 
-Vector2D operator-(const Point2D &l, const Point2D &s) noexcept;
+Vector2D operator-(const Point2D& l, const Point2D& s) noexcept;
 
-
-#endif // STRUCTURES_H
+#endif  // STRUCTURES_H
