bae28bc9322313bb329bdfa0816c434d32409a9f
Dmitriy Zolotykh <dm.zolotykh@gmail.com>
2024-02-22 11:25:21 +0300

    improve algo

--- src/graph/algo/FruchtermanReingold.cpp
+++ src/graph/algo/FruchtermanReingold.cpp
@@ -1,9 +1,20 @@
 #include "FruchtermanReingold.h"
 
-FruchtermanReingold::FruchtermanReingold(const Graph &g, double k) : g_(g)
-        , k_(k)
-        , temp_(g.size())
-        , move_to(g_.size()) {}
+FruchtermanReingold::FruchtermanReingold(const Graph &g,  size_t width, size_t height, double k) : g_(g)
+        ,k_(k)
+        ,temp_(g.size())
+        ,move_to_(g_.size()),
+        positions_(g.size()),
+        width_(width),
+        height_(height)
+        {
+            double angle = 2.0 * M_PI / g.size();
+            for (Graph::vertexT v_id = 0; v_id < g.size(); v_id++) {
+                positions_[v_id].x = cos(v_id * angle);
+                positions_[v_id].y = sin(v_id * angle);
+            }
+
+        }
 
 double FruchtermanReingold::fa(double x) const noexcept {
     return x * x / k_;
@@ -13,28 +24,63 @@ double FruchtermanReingold::fr(double x) const noexcept {
     return k_ * k_ / x;
 }
 
-void FruchtermanReingold::run(std::vector<Point2D> &positions) noexcept {
-    fill(move_to.begin(), move_to.end(), Vector2D(0, 0));
+std::vector<Point2D> FruchtermanReingold::get_positions() const noexcept {
+    return positions_;
+}
+
+void FruchtermanReingold::scale() noexcept {
+    // find current dimensions
+    double x_min = std::numeric_limits<double>::max();
+    double x_max = std::numeric_limits<double>::lowest();
+    double y_min = std::numeric_limits<double>::max();
+    double y_max = std::numeric_limits<double>::lowest();
+
+    for (Graph::vertexT v_id = 0; v_id < g_.size(); v_id++) {
+        x_min = std::min(x_min, positions_[v_id].x);
+        x_max = std::max(x_max, positions_[v_id].x);
+        y_min = std::min(y_min, positions_[v_id].y);
+        y_max = std::max(y_max, positions_[v_id].y);
+    }
+
+    double cur_width = x_max - x_min;
+    double cur_height = y_max - y_min;
+
+    // compute scale factor (0.9: keep some margin)
+    double x_scale = width_ / cur_width;
+    double y_scale = height_ / cur_height;
+    double scale = 0.9 * std::min(x_scale, y_scale);
+
+    // compute offset and apply it to every position
+    Vector2D center(x_max + x_min, y_max + y_min);
+    Vector2D offset = center / 2.0 * scale;
+    for (Graph::vertexT v_id = 0; v_id < g_.size(); v_id++) {
+        auto tmp = ((Vector2D) positions_[v_id] * scale - offset);
+        positions_[v_id] = Point2D(tmp.x + width_ / 2.0, tmp.y + height_ / 2.0);
+    }
+}
+
+void FruchtermanReingold::run_iter() noexcept {
+    fill(move_to_.begin(), move_to_.end(), Vector2D(0, 0));
 
     for (Graph::vertexT v = 0; v < g_.size(); ++v) {
         for (Graph::vertexT u = v + 1; u < g_.size(); ++u) {
-            auto delta = positions[v] - positions[u];
-            move_to[v] += (delta * fr(delta.size()) / delta.size());
-            move_to[u] -= (delta * fr(delta.size()) / delta.size());
+            auto delta = positions_[v] - positions_[u];
+            move_to_[v] += (delta * fr(delta.size()) / delta.size());
+            move_to_[u] -= (delta * fr(delta.size()) / delta.size());
         }
     }
 
     for (Graph::vertexT v = 0; v < g_.size(); ++v) {
         for (Graph::vertexT u : g_.get_neighbours(v)) {
             if (u > v) continue;
-            auto delta = positions[v] - positions[u];
-            move_to[v] -= (delta * fa(delta.size()) / delta.size());
-            move_to[u] += (delta * fa(delta.size()) / delta.size());
+            auto delta = positions_[v] - positions_[u];
+            move_to_[v] -= (delta * fa(delta.size()) / delta.size());
+            move_to_[u] += (delta * fa(delta.size()) / delta.size());
         }
     }
 
     for (Graph::vertexT v = 0; v < g_.size(); ++v) {
-        positions[v] += (move_to[v] * std::min(temp_, move_to[v].size()) / move_to[v].size());
+        positions_[v] += (move_to_[v] * std::min(temp_, move_to_[v].size()) / move_to_[v].size());
     }
 
     temp_ *= 0.99;
--- src/graph/algo/FruchtermanReingold.h
+++ src/graph/algo/FruchtermanReingold.h
@@ -4,20 +4,37 @@
 #include "../Graph.h"
 #include "../../math/Vector2D.h"
 #include "../../math/Point2D.h"
+#include <type_traits>
+
 
 class FruchtermanReingold {
 public:
-    FruchtermanReingold(const Graph& g, double k = 15.0);
-    void run(std::vector<Point2D>& positions) noexcept;
+    explicit FruchtermanReingold(const Graph& g, size_t width, size_t height, double k = 15.0);
+
+    template<typename callbackT = void(int)>
+    void run(size_t iter, callbackT callback = [](int){}) noexcept {
+        for (int i = 0; i < iter; ++i) {
+            run_iter();
+            callback(i);
+        }
+        scale();
+    }
+
+    std::vector<Point2D> get_positions() const noexcept;
 
 private:
     const Graph& g_;
     const double k_;
     double temp_;
-    std::vector<Vector2D> move_to;
+    const size_t width_;
+    const size_t height_;
+    std::vector<Vector2D> move_to_;
+    std::vector<Point2D> positions_;
 
     double fa(double x) const noexcept;
     double fr(double x) const noexcept;
+    void run_iter() noexcept;
+    void scale() noexcept;
 };
 
 
